47732758622a7362a6f086e7f857cb17f4d17a56
Thiago Hersan <thiago.hersan+github@gmail.com>
2021-03-21 19:48:05 -0400

    start frontend app

---
+++ captcha-service/src/app/index.js
@@ -0,0 +1,42 @@
+const EL = {};
+const FONT_SIZE = 48;
+
+let mFont;
+
+function preload() {
+  mFont = loadFont('./fonts/Open_Sans/OpenSans-Bold.ttf');
+  EL.container = document.getElementById('my-canvas-container');
+  EL.menu = document.getElementById('my-menu-container');
+  EL.text = document.getElementById('my-input-text');
+  EL.text.addEventListener('keyup', createCaptcha);
+}
+
+function setup() {
+  const mDim = Math.min(windowWidth, windowHeight - EL.menu.offsetHeight);
+  const mCanvas = createCanvas(mDim, mDim);
+  mCanvas.parent('my-canvas-container');
+  mCanvas.id('my-captcha-canvas');
+  mCanvas.elt.classList.add('captcha-canvas');
+  smooth();
+  noLoop();
+  textFont(mFont);
+  textSize(FONT_SIZE);
+  textAlign(CENTER, CENTER);
+}
+
+function windowResized() {
+  const mDim = Math.min(windowWidth, windowHeight - EL.menu.offsetHeight);
+  resizeCanvas(mDim, mDim);
+}
+
+
+function createCaptcha() {
+  // TODO:
+  //   - get text, measure length/height, etc
+  //   - set font-size
+  //   - place text
+  //   - distort text
+
+  background(255);
+  text(EL.text.value, 0, 0, width, height);
+}
