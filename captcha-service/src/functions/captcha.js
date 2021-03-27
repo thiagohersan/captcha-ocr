11e5de99c1519ce26d8df1e416be71eafc78027c
Thiago Hersan <thiago.hersan+github@gmail.com>
2021-03-26 22:28:38 -0400

    adds s3 to serverless... breaks everything

--- captcha-service/src/functions/captcha.js
+++ captcha-service/src/functions/captcha.js
@@ -14,7 +14,7 @@ module.exports.compare = async (event, context) => {
                                   process.env.THE_ANSWER).toString(utf8f));
 
   const success = (compareTwoStrings(phrase, answer) > 0.9);
-  const url = success ? process.env.SUCCESS_URL : process.env.FAIL_URL;
+  const url = success ? process.env.SUCCESS_URL : '';
 
   return {
     statusCode: 200,
