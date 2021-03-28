6a9baaac5d75360acc99658daef645826e384bf1
Thiago Hersan <thiago.hersan+github@gmail.com>
2021-03-28 14:31:58 -0400

    adjust seed phrases

--- captcha-service/app/js/seed.js
+++ captcha-service/app/js/seed.js
@@ -10,7 +10,7 @@ const seedPhrases = [
         'The heresy of heresies was common sense.',
         'Nothing was your own except the few cubic centimeters in your skull.',
         'Who controls the past controls the future. Who controls the present controls the past.',
-        'We know that no one ever seizes power with the intention of relinquishing it.',
+        'No one ever seizes power with the intention of relinquishing it.',
         'Power is in tearing human minds to pieces and putting them together again in new shapes of your own choosing.',
         'History has stopped. Nothing exists except an endless present in which the Party is always right.',
         'You will be hollow. We shall squeeze you empty, and then we shall fill you with ourselves.',
@@ -23,11 +23,11 @@ const seedPhrases = [
         'O bom senso era a heresia das heresias.',
         'Nada pertencia ao indivíduo, com exceção de alguns centímetros cúbicos dentro do crânio.',
         'Quem controla o passado, controla o futuro; quem controla o presente controla o passado',
-        'Sabemos que ninguém jamais toma o poder com a intenção de largá-lo.',
-        'O poder está em se despedaçar os cérebros humanos e tornar a juntá-los da forma que se bem entender.',
+        'Ninguém jamais toma o poder com a intenção de largá-lo.',
+        'O poder está na capacidade de despedaçar os humanos e tornar a juntá-los da forma que se bem entender.'
         'A história parou. Nada existe, exceto um presente sem-fim no qual o Partido tem sempre razão.',
         'Serás oco. Havemos de te expremer, te deixar vazio, e então saberemos como te encher.',
-        'Para guardar segredo é preciso escondê-lo também da própria consciência.',
+        'Para guardar um segredo é preciso escondê-lo também da própria consciência.',
         'O propósito de fazer guerra é estar sempre em melhor posíção para fazer outra guerra.',
         'GUERRA É PAZ. LIBERDADE É ESCRAVIDÃO. IGNORANCIA É FORÇA.'
       ]
