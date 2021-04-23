b56f6822c9f0c1dcc05f5b038cf36e4482971182
Thiago Hersan <thiago.hersan+github@gmail.com>
2021-04-22 23:42:05 -0400

    script to split 1984 text into 40-char phrases

---
+++ 1984/scripts/split_1984.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+from re import match, split
+from os import listdir, makedirs
+from os.path import isfile, exists, join, dirname, realpath
+
+mydir = dirname(realpath(__file__))
+myoutpath = join(mydir, '..', 'out')
+
+if not exists(myoutpath):
+  makedirs(myoutpath)
+
+myinpath = join(mydir, '..')
+fpath = join(myinpath, '1984_gutenberg.txt')
+
+lines = []
+
+with open(fpath) as fpi:
+  text = (fpi.read()
+          .replace('\r\n', ' ')
+          .replace('\n', ' ')
+          .replace('    ', ' ')
+          .replace('   ', ' ')
+          .replace('  ', ' ')
+          .replace('--', ', '))
+
+  words = text.strip().split(' ')
+
+  word_idx = 0
+  thisPhrase = ''
+
+  while(words[word_idx] != 'PART'):
+    word_idx += 1
+
+  while(word_idx < len(words)):
+    thisPhrase += words[word_idx] + ' '
+
+    if(len(thisPhrase) > 40):
+      lines.append(thisPhrase.strip())
+      thisPhrase = ''
+    word_idx += 1
+
+  if(len(thisPhrase) > 0):
+    lines.append(thisPhrase.strip())
+
+
+with open(join(myoutpath, 'out_' + '1984.js'), 'w') as fpo:
+  fpo.write('const phrases1984 = [\n')
+  for l in lines:
+    fpo.write('  \'' + l.replace('\'', '\\\'') + '\',\n')
+  fpo.write('];\n')
